90f4e2457fdaf4722fd0c5f09b2fc0e9dc7a6a09
kptdaaron <kptdaaron@gmail.com>
2022-05-25 15:08:11 +0800

    Added CloudFront section to main.tf

--- main.tf
+++ main.tf
@@ -215,3 +215,63 @@ resource "aws_s3_bucket_public_access_block" " s3_block_access" {
 locals {
     s3_origin_id = "s3Origin"
 }
+
+## CloudFront Distribution
+
+# origin access identity for distribution
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment = "cloudfront distribution identity"
+}
+
+# distribution
+
+resource "aws_cloudfront_distribution" "distribution" {
+    origin {
+        # this is the domain name of s3 bucket which we created
+        domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+        # here we are using that s3 origin id
+        origin_id = local.s3_origin_id
+
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    comment = "cloudfront for static content"
+
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
+
+        forwarding_values {
+            query_string = False
+            
+            cookies {
+                forward = "none"
+            }
+        }
+
+        # here we are defining to redirect http traffic to https
+        viewer_protocol_policy = "redirect-to-https"
+        main_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    # here we are defining geo_restriction as none
+    # this is a very useful attribute. It can be used when we have to block content on a certain geographical region
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+}
